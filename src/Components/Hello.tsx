e06e3d4a21e63167afe82a2748df13368db7024c
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-04 23:59:11 +0600

    useRef update  using ts

--- src/Components/Hello.tsx
+++ src/Components/Hello.tsx
@@ -4,6 +4,7 @@ type Info = {
   name: string;
   age: number;
 };
+
 type HelloProps = {
   userInfo: Info;
 };
@@ -17,13 +18,14 @@ const Hello = ({ userInfo }: HelloProps) => {
   const [user, setUser] = useState<User | null>(null);
   const [count, setCount] = useState<number>(0);
   const [inputValue, setInputValue] = useState<string>("");
-  const inputRef = useRef<null>(null);
 
-  useEffect((): void => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
     const fetchData = async () => {
       const data: User = {
         name: "Hasan",
-        email: "hasan@gamil.com",
+        email: "hasan@gmail.com",
       };
       setUser(data);
     };
@@ -33,27 +35,26 @@ const Hello = ({ userInfo }: HelloProps) => {
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
+
   return (
     <div>
-      <div>{`Hello ${userInfo.name}. You are ${userInfo.age} Years Old `}</div>
-      <div>
-        {user && (
-          <div>
-            Name : {user.name} - Email: {user.email}
-          </div>
-        )}
-      </div>
+      <div>{`Hello ${userInfo.name}. You are ${userInfo.age} Years Old`}</div>
+
+      {user && (
+        <div>
+          Name : {user.name} - Email: {user.email}
+        </div>
+      )}
+
       <div>{count}</div>
-      <div>
-        <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-      </div>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Increment
+      </button>
 
       <div>
         <input
           ref={inputRef}
           type="text"
-          name=""
-          id=""
           onChange={handleInputChange}
           value={inputValue}
         />
