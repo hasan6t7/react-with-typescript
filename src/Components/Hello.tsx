5b438213e4fc69ed2a97f6d55b78ec17f02625c9
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-04 22:33:03 +0600

    useState and useEffect using ts

--- src/Components/Hello.tsx
+++ src/Components/Hello.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 type Info = {
   name: string;
   age: number;
@@ -6,11 +8,34 @@ type HelloProps = {
   userInfo: Info;
 };
 
+type User = {
+  name: string;
+  email: string;
+};
+
 const Hello = ({ userInfo }: HelloProps) => {
-  
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(():void => {
+    const fetchData = async () => {
+      const data: User = {
+        name: "Hasan",
+        email: "hasan@gamil.com",
+      };
+      setUser(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <div>{`Hello ${userInfo.name}. You are ${userInfo.age} Years Old `}</div>
+      <div>
+        {user && (
+          <div>
+            Name : {user.name} - Email: {user.email}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
